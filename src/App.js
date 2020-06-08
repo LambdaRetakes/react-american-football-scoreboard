7176c009dfe8f34ce2554eb6989a0255f064d7c8
Khari <kharris2k16@gmail.com>
2020-06-08 12:50:20 -0700

    fixed the timer

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [ourBoisScore, setOurBoisScore] = useState(0);
   const [badBoisScore, setBadBoisScore] = useState(0);
-  // const [time] = useState(Date.Now)
+  const [time, setTime] = useState(new Date().getHours() + ":" + new Date().getMinutes() + ":" +  new Date().getSeconds())
+
+  const getTime = () => {
+    setTime(new Date().getHours() + ":" + new Date().getMinutes() + ":" +  new Date().getSeconds())
+  }
+  setTimeout(getTime, 1000);
 
   const weScored = e => {
     setOurBoisScore(ourBoisScore + 10);
@@ -28,14 +33,14 @@ function App() {
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">The Homies</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
             <div className="home__score">{ourBoisScore}</div>
           </div>
-          {/* <div className="timer">{time}</div> */}
+          <div className="timer">{time}</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">Like 3 blocks away</h2>
             <div className="away__score">{badBoisScore}</div>
           </div>
         </div>
